05b51aa37edbb58abb2f32c43f49fa45f0b029b9
k3enanb4syir <mouchbobby@gmail.com>
2024-09-29 14:57:27 +0700

    Add files via upload
    Make a program to input a certain number of student's information such as name, nim, gender. The number of inputted students is up to the user, therefore the program should ask whether the user want to input again or finish.
    In the end of the program (after inputting information), it will show the number of the male and female students.
    
    Must use: looping, conditional, array, struct.

---
+++ Assignment_3/Task/programming_assignment3.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct data
+{
+	string name;
+	int nim;
+	string gender;
+	int *p;   //pointer (but not used, just to show that it can be put in the struct)
+};
+int main ()
+{
+	struct data mahasiswa [10];
+	int i = 0;
+	char input = 1;
+
+
+
+	cout << "Do you want to add any new student? (YES=1 | NO=0)" << endl;
+	cin >> input;
+	cin.ignore();
+	
+	while (input == '1') {
+		cout << "Enter name of student" << endl;
+		getline(cin, mahasiswa[i].name);
+		
+		
+		cout << "Enter NIM" << endl;
+		cin >> mahasiswa[i].nim;
+		
+		cout << "Enter gender of student (MALE | FEMALE)" <<endl;
+		cin >> mahasiswa[i].gender;
+		while (mahasiswa[i].gender!= "MALE" && mahasiswa[i].gender!= "FEMALE") {
+			cout << "Error input, please enter MALE OR FEMALE" <<endl;
+			cin >> mahasiswa[i].gender;
+		}
+		
+		cout << "----------------------" << endl;
+		cout << "Name: " << mahasiswa[i].name << endl;
+		cout << "----------------------" << endl;
+		cout << "NIM: " << mahasiswa[i].nim << endl;
+		cout << "----------------------" << endl;
+		cout << "Gender: " << mahasiswa[i].gender << endl;
+		cout << "----------------------" << endl;
+
+
+
+
+		cout << "Do you want to continue? (YES = 1 | NO = 0)" << endl;
+		cin >> input;
+		cin.ignore();
+		i = i + 1;
+
+
+
+	}
+    cout << "No new student will be added (END)";
+
+
+}
